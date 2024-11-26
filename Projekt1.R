0c9bb299a1dc08a129232071e51070f4c6124a39
Tynka <martyna05p@gmail.com>
2024-11-26 19:33:23 +0100

    wszystko dodane do word, drobne zmiany w kodzie (np. tabele rozkładu liczebności są razem z procentami, zmiana kolorów, nazw osi)

--- Projekt1.R
+++ Projekt1.R
@@ -1,28 +1,25 @@
 attach(dane_do_zestawu_17)
 #ZADANIE 1
 #ROZKŁAD LICZEBNOŚCI-OSZCZĘDNOŚCI
-rozklad_l_w <- data.frame(
-  #Nr=1:length(unique(Oszczednosci)),
-  Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
-  Liczność=sort(as.numeric(table(Oszczednosci)))
-)
-rozklad_l_w
 
-#ROZKŁAD LICZEBNOŚCI PROCENTOWY-OSZCZĘDNOŚCI
-Procent=percent(sort(as.numeric(table(Oszczednosci))/306), accuracy=0.01)
+procent <- percent(sort(as.numeric(table(Oszczednosci)) / 306), accuracy = 0.01)
 rozklad_l_w_procent <- data.frame(
-  #Nr=1:length(unique(Oszczednosci)),
-  Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
-  Procent=(Procent)
+  Oszczędności = c("Mniej niż 5 procent", "5-10 procent", "10-20 procent", "Powyżej 20 procent"),
+  Liczność = sort(as.numeric(table(Oszczednosci))),
+  Procent = procent # Użycie poprawnej nazwy zmiennej
 )
-rozklad_l_w_procent
+print(rozklad_l_w_procent)
+obrot<-t(rozklad_l_w_procent)
+
+# Eksport do CSV
+write.csv(obrot, "rozkład_wszyscy.csv", row.names = FALSE, fileEncoding = "UTF-8")
 
 
 #WYKRES KOLUMNOWY ROZKŁADU
 procent=(percent(sort(as.numeric(table(Oszczednosci))/306), accuracy=0.01))
 wysokosc=(sort(as.numeric(table(Oszczednosci))/306*100))
-wykres_w=barplot((sort(table(Oszczednosci)/306*100)), main = "Oszczędności",
-      xlab = "Procent budżetu przeznaczony na oszczędności", ylab = "Procent osób [%]", col = "#42a683",
+wykres_w=barplot((sort(table(Oszczednosci)/306*100)), main = "Procent dochodu przeznaczany przez badanych na oszczędności",
+      xlab = "Procent dochodu gospodarstwa domowego [%]", ylab = "Procent osób [%]", col = c("#5B2125FF", "#8D3431FF", "#BF542EFF", "#E9A800FF"),
       names.arg = c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
       ylim=c(0,55))
 text(wykres_w, wysokosc+2, labels=procent ,cex=1) 
@@ -32,27 +29,24 @@ wykres_w
 #ZADANIE 2
 #ROZKŁAD LICZEBNOŚCI-OSZCZĘDNOŚCI _ KOBIETY
 kobiety <- subset(dane_do_zestawu_17, Plec == "K")
-rozklad_l_k <- data.frame(
-  Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
-  Liczność=sort(as.numeric(table(kobiety$Oszczednosci)))
-)
-rozklad_l_k
 
-#ROZKŁAD LICZEBNOŚCI PROCENTOWY _ KOBIETY
 rozklad_l_k_procent <- data.frame(
   #Nr=1:length(unique(Oszczednosci)),
   Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
+  Liczność=sort(as.numeric(table(kobiety$Oszczednosci))),
   Procent=(percent(sort(as.numeric(table(kobiety$Oszczednosci))/306), accuracy=0.01))
 )
 
-rozklad_l_k_procent
+rozkl_k=t(rozklad_l_k_procent)
+write.csv(rozkl_k, "rozkład_kobiety.csv", fileEncoding = "UTF-8")
+
 
 #WYKRES KOLUMNOWY ROZKŁADU
 #oszcz_k=sort(table(kobiety$Oszczednosci)/306*100)
 procent=(percent(sort(as.numeric(table(kobiety$Oszczednosci))/306), accuracy=0.01))
 wysokosc=(sort(as.numeric(table(kobiety$Oszczednosci))/306*100))
-wykres_k=barplot(sort(table(kobiety$Oszczednosci)/306*100), main = "Oszczędności kobiet",
-        xlab = "Procent budżetu przeznaczony na oszczędności", ylab = "Procent kobiet [%]", col = "pink",
+wykres_k=barplot(sort(table(kobiety$Oszczednosci)/306*100), main = "Procent dochodu przeznaczany przez kobiety na oszczędności",
+        xlab = "Procent dochodu gospodarstwa domowego [%]", ylab = "Procent kobiet [%]", col = c("#5B2125FF", "#8D3431FF", "#BF542EFF", "#E9A800FF"),
         names.arg = c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
         ylim=c(0,30))
 text(wykres_k, wysokosc+2, labels=procent ,cex=1) 
@@ -60,25 +54,22 @@ wykres_k
 
 #ROZKŁAD LICZEBNOŚCI-OSZCZĘDNOŚCI _ MEZCZYZNI
 mezczyzni <- subset(dane_do_zestawu_17, Plec == "M")
-rozklad_l_m <- data.frame(
-  Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
-  Liczność=sort(as.numeric(table(mezczyzni$Oszczednosci)))
-)
-rozklad_l_m
 
-#ROZKŁAD LICZEBNOŚCI PROCENTOWY _ MEZCZYZNI
 rozklad_l_m_procent <- data.frame(
   #Nr=1:length(unique(Oszczednosci)),
   Oszczędności=c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
+  Liczność=sort(as.numeric(table(mezczyzni$Oszczednosci))),
   Procent=(percent(sort(as.numeric(table(mezczyzni$Oszczednosci))/306), accuracy=0.01))
 )
 rozklad_l_m_procent
+rozk_m=t(rozklad_l_m_procent)
+write.csv(rozk_m, "rozkład_m.csv", fileEncoding = "UTF-8")
 
 #WYKRES KOLUMNOWY ROZKŁADU
 procent=(percent(sort(as.numeric(table(mezczyzni$Oszczednosci))/306), accuracy=0.01))
 wysokosc=(sort(as.numeric(table(mezczyzni$Oszczednosci))/306*100))
-wykres_m=barplot(sort(table(mezczyzni$Oszczednosci)/306*100), main = "Oszczędności mężczyzn",
-            xlab = "Procent budżetu przeznaczony na oszczędności", ylab = "Procent mężczyzn [%]", col = "lightblue",
+wykres_m=barplot(sort(table(mezczyzni$Oszczednosci)/306*100), main = "Procent dochodu przeznaczany przez mężczyzn na oszczędności",
+            xlab = "Procent dochodu gospodarstwa domowego [%]", ylab = "Procent mężczyzn [%]", col=c("#5B2125FF", "#8D3431FF", "#BF542EFF", "#E9A800FF"),
             names.arg = c("Mniej niż 5 procent", "5-10 procent","10-20 procent", "Powyżej 20 procent"),
             ylim=c(0,30))
 text(wykres_m, wysokosc+2, labels=procent ,cex=1) 
@@ -87,13 +78,15 @@ wykres_m
 
 #ZADANIE 3 - DYSTRYBUANTA
 dyst_k=subset(dane_do_zestawu_17, Plec == "K"& Zamieszkanie=="Miasto"& Ubezpieczenie=="Tak")
-dystrybuanta=plot(ecdf(dyst_k$Wiek), main="Dystrybuanta kobiet",col="#CF8580")
+dystrybuanta=plot(ecdf(dyst_k$Wiek), main="Dystrybuanta empiryczna dla wieku \nkobiet ubezpieczonych i mieszkających w mieście",col="#8D3431FF")
 
 #ZADANIE 4 - TABLICA DWUDZIELCZA 
 tablica_dwudzielcza <- data.frame(table(dane_do_zestawu_17$Zamieszkanie, dane_do_zestawu_17$Wyksztalcenie))
 tablica_dwudzielcza_df <- as.data.frame(tablica_dwudzielcza)
 colnames(tablica_dwudzielcza_df) <- c("Zamieszkanie", "Wykształcenie", "Liczność")
 print(tablica_dwudzielcza_df)
+
+write.csv(tablica_dwudzielcza_df, "tabrozd.csv", fileEncoding = "UTF-8")
 
 #ZADANIE 5
 #UBEZPIECZA SIĘ
@@ -164,6 +157,16 @@ mediana_wiek <- aggregate(Wiek ~ Zamieszkanie + Wiedza, data = dane_do_zestawu_1
 print(srednia_wiek)
 print(mediana_wiek)
 
+write.csv(mediana_wiek, "mediana.csv", row.names=FALSE, fileEncoding = "UTF-8")
 
+
+# Wyznaczenie średniej z zaokrągleniem do 2 miejsc po przecinku
+srednia_wiek <- aggregate(Wiek ~ Zamieszkanie + Wiedza, 
+                          data = dane_do_zestawu_17, 
+                          FUN = function(x) round(mean(x, na.rm = TRUE), 2))
+
+# Wyświetlenie wyniku
+print(srednia_wiek)
+write.csv(srednia_wiek, "srednia.csv", row.names=FALSE, fileEncoding = "UTF-8")
 
 
